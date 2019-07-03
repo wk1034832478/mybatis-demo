7e9e75d002c83ef0a0254576821d6526e09a105e
unknown <1034832478@qq.com>
2019-07-03 14:59:03 +0800

    '动态sql测试'

--- src/main/java/org/mybatis/example/BlogMapper.java
+++ src/main/java/org/mybatis/example/BlogMapper.java
@@ -1,6 +1,10 @@
 package org.mybatis.example;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author 王凯
@@ -10,6 +14,10 @@ public interface BlogMapper {
 
     Blog selectBlog(int id);
 
-    @Select("SELECT * FROM blog WHERE id = #{id}")
+    @Select("SELECT * FROM wangkai.blog WHERE id = #{id}")
     Blog selectBlogThroughAnnotation(int id);
+
+    Integer selectByProcedure(Map<String, Object> map);
+
+    List<Blog> selectByName(@Param("name") String name);
 }
--- src/test/java/com/haixi/AppTest.java
+++ src/test/java/com/haixi/AppTest.java
@@ -3,6 +3,9 @@ package com.haixi;
 import static org.junit.Assert.assertTrue;
 
 import com.haixi.log.AppLog;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -15,6 +18,9 @@ import org.mybatis.example.BlogMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -33,8 +39,10 @@ public class AppTest
      */
     @Before
     public void testBefore() {
+        // 配置文件位置
         String resource = "mybatis-config.xml";
         try {
+            // 获取配置文件资源
             InputStream inputStream = Resources.getResourceAsStream(resource);
             // 一旦创建了 SqlSessionFactory，就不再需要它了， 需要保持在局部方法更可靠
             this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
@@ -56,13 +64,59 @@ public class AppTest
     public void test2() {
         sqlSession = this.sqlSessionFactory.openSession();
         BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
-        Blog blog = blogMapper.selectBlogThroughAnnotation(1);
+        Blog blog = blogMapper.selectBlogThroughAnnotation(2);
         AppLog.log( blog );
     }
 
     @Test
     public void test3() {
         sqlSession = this.sqlSessionFactory.openSession();
+    }
+
+    @Test
+    public void test4() {
+        FileSystem fs;
+        //方式1通过配置来获取fs
+        /*Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://node1:9000");
+        FileSystem fs = FileSystem.get(conf);
+        System.setProperty("HADOOP_USER_NAME", "root");*/
+        //方式2直接获取fs
+        Configuration conf = new Configuration();
+        conf.set("dfs.replication", "1");
+        conf.set("dfs.block.size", "64m");
+        try {
+            AppLog.log( "正在连接hdfs" );
+            fs = FileSystem.get(new URI("hdfs://119.162.211.98:9000"), conf, "wangkai");
+            AppLog.log( fs.mkdirs( new Path("/wangkai2")) );
+            AppLog.log( "hdfs操作完成" );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试调用存储过程， 定义格式如下
+     * call query_blog_by_id(#{id,mode=IN,jdbcType=INTEGER},#{name,mode=OUT,jdbcType=VARCHAR})
+        */
+    @Test
+    public void test5() {
+        sqlSession = this.sqlSessionFactory.openSession();
+        BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
+        String a = "" ;
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 2);
+        blogMapper.selectByProcedure(map);
+        AppLog.log(  blogMapper.selectByProcedure(map) );
+        AppLog.log( map );
+    }
+
+
+    @Test
+    public void test6() {
+        sqlSession = this.sqlSessionFactory.openSession();
+        BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
+        System.out.println( blogMapper.selectByName("wangkai") );
     }
 
     @After
